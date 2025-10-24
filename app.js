765fa83d4a6de666053053c018c1809ae5ac36dc
Andrej062 <andrej.gluchariov@gmail.com>
2025-10-24 10:30:06 +0200

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ try {
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.use((req, res, next) => {
